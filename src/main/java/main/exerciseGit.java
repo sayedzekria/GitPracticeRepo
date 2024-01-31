bf9fb6a2270155759a0c725353b235ceddc1fa5f
sayed zekria <sayed.zekria.sayedi@tekschool.us>
2024-01-31 13:25:50 -0600

    new practice project

---
+++ src/main/java/main/exerciseGit.java
@@ -0,0 +1,11 @@
+package main;
+
+public class exerciseGit {
+    public static void main(String[] args) {
+        System.out.println("i love coding");
+        System.out.println("i love java");
+        System.out.println("i love selenium");
+        System.out.println("i love git");
+        System.out.println("i love C++");
+    }
+}
